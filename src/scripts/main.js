047f7a914476bf63144c307a1a8ca029db2cde7a
ElderSud <eldercuases@gmail.com>
2025-03-28 16:31:39 -0500

    Primeros estilos Mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,3 +3,44 @@
  * Este bloque de código contiene la funcionalidad principal
  * que define el comportamiento del programa.
  */
+
+import { stays } from "./stays.js";
+import { loadCharacters } from "./utils.js";
+
+// nodos html
+const characterList = document.querySelector("#character-list");
+const searchInput = document.querySelector("#search-input");
+const searchButton = document.querySelector("#search-button");
+let limit = 5;
+
+loadCharacters(stays, characterList);
+
+/*filterButtons.addEventListener("click", (event) => {
+  let target = event.target; // elemento sobre el cual el usuario hizo click
+  let tagName = target.tagName; // nombre de la etiqueta precionada por el usuario
+  if (tagName === "BUTTON") {
+    // validacion para verficar que usuario haya hecho click sobre un boton.
+    let data = characters;
+    if (target.value !== "all") {
+      data = characters.filter(
+        (personaje) =>
+          personaje.status.toLocaleLowerCase() ===
+          target.value.toLocaleLowerCase()
+      );
+    }
+    loadCharacters(data, characterList);
+    activeButton(target);
+  }
+});*/
+
+/*searchButton.addEventListener("click", (e) => {
+  searchCharacter(searchInput, characters, characterList);
+});
+
+loadMore.addEventListener("click", () => {
+  limit += 5;
+  loadCharacters(characters.slice(0, limit), characterList);
+  if (limit === characters.length) {
+    loadMore.remove();
+  }
+});*/
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -3,3 +3,66 @@
  * Este archivo contiene funciones auxiliares que serán utilizadas y llamadas
  * desde el archivo principal para realizar varias operaciones.
  */
+
+export function loadCharacters(array, elementHTML) {
+  elementHTML.innerHTML = "";
+
+  array.forEach(function (item) {
+    const template = characterTemplate(
+      item.photo,
+      item.superHost,
+      item.type,
+      item.bed,
+      item.title,
+      item.rating
+    );
+
+    elementHTML.innerHTML += template;
+  });
+}
+
+export function characterTemplate(img, superHost, name, bed, status, stars) {
+  if (superHost === true) {
+    const template = `
+        <li class="flex flex-col rounded-lg shadow-md overflow-hidden w-full pb-4 sm:flex-row sm:h-60 sm:pb-0">
+
+            <img src="${img}" alt="${name}" class="w-full object-cover sm:w-1/2 rounded-lg">
+
+            <div aria-label="card content" class="flex justify-between p-4">
+                <h3 class="text-xl font-semibold">${name}</h3>
+                <span class=" flex items-center space-x-2"><svg xmlns="http://www.w3.org/2000/svg" fill="#eb5757" viewBox="0 0 24 24" stroke-width="1.5" stroke="#eb5757" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+</svg>${stars}</span>
+</div>
+                <p class="text-gray-600 font-bold relative before:absolute before:size-2 before:bg-gray-300 ml-4 before:-left-4 before:top-2.5 before:rounded-full">
+                    <span>${status}</span> - 
+                </p>
+
+                 
+            
+        </li>
+    `;
+    return template;
+  } else {
+    const template = `
+        <li class="flex flex-col rounded-lg shadow-md overflow-hidden w-full pb-4 sm:flex-row sm:h-60 sm:pb-0">
+
+            <img src="${img}" alt="${name}" class="w-full object-cover sm:w-1/2 rounded-lg">
+
+            <div aria-label="card content" class="flex justify-between p-4">
+                <h3 class="text-xl font-semibold">${name}</h3>
+                <span class=" flex items-center space-x-2"><svg xmlns="http://www.w3.org/2000/svg" fill="#eb5757" viewBox="0 0 24 24" stroke-width="1.5" stroke="#eb5757" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+</svg>${stars}</span>
+</div>
+                <p class=" text-gray-600 font-bold relative before:absolute before:size-2 before:bg-gray-300 ml-4 before:-left-4 before:top-2.5 before:rounded-full">
+                    <span>${status}</span> - 
+                </p>
+
+                 
+            
+        </li>
+    `;
+    return template;
+  }
+}
