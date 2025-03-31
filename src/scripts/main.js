63e6f60b1ad2f45d87c68b3afe9a0cc2b3204842
ElderSud <eldercuases@gmail.com>
2025-03-31 06:46:19 -0500

    correcciones de codigo y funciones faltantes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,42 +5,139 @@
  */
 
 import { stays } from "./stays.js";
-import { loadCharacters } from "./utils.js";
+import { loadCharacters, toggle, loadLocation } from "./utils.js";
 
 // nodos html
 const characterList = document.querySelector("#character-list");
-const searchInput = document.querySelector("#search-input");
-const searchButton = document.querySelector("#search-button");
-let limit = 5;
+const locationInput = document.querySelector("#modal-search-input");
+const locationOpts = document.querySelector("#location-opts");
+const inputGuest = document.querySelector("#modal-guest-input");
+const ButtonGuest = document.querySelector("#buttons-guest");
+const display = document.querySelector("#display");
+const ButtonGuestN = document.querySelector("#buttons-guest-n");
+const displayN = document.querySelector("#display-n");
+const openBtn = document.querySelector("#open");
+const modal = document.querySelector("#modal");
+const navModal = document.querySelector("#nav-modal");
+const conMod = document.querySelector("#contador-modal");
+const modalSearch = document.querySelector("#search-modal-button");
+const filteredCity = document.querySelector("#filtered-city");
 
+//Cards
 loadCharacters(stays, characterList);
 
-/*filterButtons.addEventListener("click", (event) => {
-  let target = event.target; // elemento sobre el cual el usuario hizo click
-  let tagName = target.tagName; // nombre de la etiqueta precionada por el usuario
-  if (tagName === "BUTTON") {
-    // validacion para verficar que usuario haya hecho click sobre un boton.
-    let data = characters;
-    if (target.value !== "all") {
-      data = characters.filter(
-        (personaje) =>
-          personaje.status.toLocaleLowerCase() ===
-          target.value.toLocaleLowerCase()
-      );
-    }
-    loadCharacters(data, characterList);
-    activeButton(target);
-  }
-});*/
+//locations
 
-/*searchButton.addEventListener("click", (e) => {
-  searchCharacter(searchInput, characters, characterList);
+loadLocation(stays, locationOpts);
+
+//lista de ciudades buscador
+
+locationInput.addEventListener("click", () => {
+  if (!conMod.classList.contains("hidden")) {
+    conMod.classList.add("hidden");
+    locationOpts.classList.add("block");
+  }
+  locationOpts.classList.toggle("hidden");
 });
 
-loadMore.addEventListener("click", () => {
-  limit += 5;
-  loadCharacters(characters.slice(0, limit), characterList);
-  if (limit === characters.length) {
-    loadMore.remove();
+locationOpts.addEventListener("click", (e) => {
+  const { target } = e;
+  const { tagName } = target;
+  if (tagName === "SPAN") {
+    locationInput.value = target.textContent;
+    locationOpts.classList.toggle("hidden");
   }
-});*/
+});
+
+//Desplegar contador
+
+inputGuest.addEventListener("click", () => {
+  if (!locationOpts.classList.contains("hidden")) {
+    locationOpts.classList.add("hidden");
+    conMod.classList.add("block");
+  }
+  conMod.classList.toggle("hidden");
+});
+
+//Contador de guests
+
+ButtonGuest.addEventListener("click", (e) => {
+  e.preventDefault();
+  const { target } = e;
+  const { tagName } = target;
+  const currentDisplay = parseInt(display.textContent) || 0;
+  const currentInputValue = parseInt(inputGuest.value) || 0;
+
+  if (tagName === "BUTTON") {
+    if (target.value === "-") {
+      if (currentDisplay === 0) {
+        return;
+      }
+      display.textContent = currentDisplay - 1;
+      inputGuest.value = currentInputValue - 1 + " guests";
+    } else if (target.value === "+") {
+      display.textContent = currentDisplay + 1;
+      inputGuest.value = currentInputValue + 1 + " guests";
+    }
+  }
+});
+
+ButtonGuestN.addEventListener("click", (e) => {
+  e.preventDefault();
+  const { target } = e;
+  const { tagName } = target;
+  const currentDisplay = parseInt(displayN.textContent) || 0;
+  const currentInputValue = parseInt(inputGuest.value) || 0;
+
+  if (tagName === "BUTTON") {
+    if (target.value === "-") {
+      if (currentDisplay === 0) {
+        return;
+      }
+      displayN.textContent = currentDisplay - 1;
+      inputGuest.value = currentInputValue - 1 + " guests";
+    } else if (target.value === "+") {
+      displayN.textContent = currentDisplay + 1;
+      inputGuest.value = currentInputValue + 1 + " guests";
+    }
+  }
+});
+
+//Abrir y cerrar modal
+navModal.addEventListener("click", toggle);
+openBtn.addEventListener("click", toggle);
+
+modal.addEventListener("click", (e) => {
+  if (e.target.id === "modal") {
+    toggle();
+  }
+});
+
+//Filtrado
+
+modalSearch.addEventListener("click", (e) => {
+  let cityValue = locationInput.value.toLowerCase().split(",")[0];
+  let guestValue = parseInt(inputGuest.value) || 0;
+  let filtered = stays;
+
+  if (cityValue !== "" && guestValue === 0) {
+    filtered = stays.filter((stay) => stay.city.toLowerCase() === cityValue);
+  } else if (cityValue === "" && guestValue > 0) {
+    filtered = stays.filter((stay) => stay.maxGuests >= guestValue);
+  } else if (cityValue !== "" && guestValue >= 0) {
+    filtered = stays.filter(
+      (stay) =>
+        stay.maxGuests >= guestValue && stay.city.toLowerCase() === cityValue
+    );
+  }
+  //Añadir el nombre de la ciudad al titulo para saber en que ciudad se estan mostrando lso resultados.
+  // Solo actualizar el span si hay resultados
+  if (filtered.length > 0) {
+    filteredCity.textContent =
+      cityValue.charAt(0).toUpperCase() + cityValue.slice(1) + " "; // Capitaliza la primera letra
+  } else {
+    filteredCity.textContent = ""; // Deja el span vacío si no hay resultados
+  }
+
+  loadCharacters(filtered, characterList);
+});
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -6,13 +6,16 @@
 
 export function loadCharacters(array, elementHTML) {
   elementHTML.innerHTML = "";
+  let contador = array.length; //cuenta la cantidad de tarjetas que se estan mostrando
+  document.querySelector("#count").textContent =
+    contador >= 12 ? 12 + "+ " : contador + " ";
 
   array.forEach(function (item) {
     const template = characterTemplate(
       item.photo,
       item.superHost,
       item.type,
-      item.bed,
+      item.beds,
       item.title,
       item.rating
     );
@@ -21,51 +24,84 @@ export function loadCharacters(array, elementHTML) {
   });
 }
 
-export function characterTemplate(img, superHost, name, bed, status, stars) {
+export function characterTemplate(img, superHost, name, beds, status, stars) {
+  let newBed = "";
+  if (beds === null) {
+    newBed = "hidden";
+  }
   if (superHost === true) {
     const template = `
-        <li class="flex flex-col rounded-lg shadow-md overflow-hidden w-full pb-4 sm:flex-row sm:h-60 sm:pb-0">
-
-            <img src="${img}" alt="${name}" class="w-full object-cover sm:w-1/2 rounded-lg">
-
-            <div aria-label="card content" class="flex justify-between p-4">
-              <div class="flex items-center space-x-2">
-            <span class=" text-gray-600 font-bold border border-gray-600 px-1 py-1 rounded-full ">SUPERHOST</span>
-                <h3 class="text-gray-600 text-xl font-semibold">${name}</h3>
+        <li class="flex flex-col overflow-hidden w-full pb-4 md:text-xs">
+              <figure class="w-full overflow-hidden h-60 rounded-lg">
+            <img src="${img}" alt="${name}" class="w-full h-full object-cover rounded-lg block ">
+             </figure >
+            <div aria-label="card content" class="flex justify-between py-4 px-1 flex-grow ">
+              <div class="flex items-center space-x-1">
+            <span class=" text-gray-600 font-bold border border-gray-600 px-1 py-1 rounded-full lg:text-xs">SUPERHOST</span>
+                <h3 class="text-gray-600 text-xl md:text-xs font-semibold">${name}<span class="${newBed}">.${beds} beds</span></h3>
                 </div>  
-                <span class=" flex items-center space-x-2"><svg xmlns="http://www.w3.org/2000/svg" fill="#eb5757" viewBox="0 0 24 24" stroke-width="1.5" stroke="#eb5757" class="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-</svg>${stars}</span>
-</div>
-                <p class="text-gray-600 font-bold relative before:absolute before:size-2 before:bg-gray-300 ml-4 before:-left-4 before:top-2.5 before:rounded-full">
-                    <span>${status}</span> - 
-                </p>
-
-                 
-            
+                <span class=" flex items-center space-x-2 pl-1"><svg xmlns="http://www.w3.org/2000/svg" fill="#eb5757" viewBox="0 0 24 24" stroke-width="1.5" stroke="#eb5757" class="size-4">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                 </svg>${stars}</span>
+                </div>
+                <p class="text-gray-900 font-bold relative px-2">
+                    <span>${status}</span>  
+                </p>    
         </li>
     `;
     return template;
   } else {
     const template = `
-        <li class="flex flex-col rounded-lg shadow-md overflow-hidden w-full pb-4 sm:flex-row sm:h-60 sm:pb-0">
-
-            <img src="${img}" alt="${name}" class="w-full object-cover sm:w-1/2 rounded-lg">
-
-            <div aria-label="card content" class="flex justify-between p-4">
-                <h3 class=" text-gray-600 text-xl font-semibold">${name}</h3>
-                <span class=" flex items-center space-x-2"><svg xmlns="http://www.w3.org/2000/svg" fill="#eb5757" viewBox="0 0 24 24" stroke-width="1.5" stroke="#eb5757" class="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-</svg>${stars}</span>
-</div>
-                <p class=" text-gray-600 font-bold relative before:absolute before:size-2 before:bg-gray-300 ml-4 before:-left-4 before:top-2.5 before:rounded-full">
-                    <span>${status}</span> - 
+        <li class="flex flex-col overflow-hidden w-full pb-4 md:text-xs ">
+            <figure class="w-full overflow-hidden h-60 rounded-lg">
+            <img src="${img}" alt="${name}" class="w-full h-full object-cover rounded-lg block">
+             </figure >
+            <div aria-label="card content" class="flex justify-between py-4 px-2 flex-grow">
+                <h3 class=" text-gray-600 text-xl md:text-xs font-semibold">${name}<span class="${newBed}">.${beds} beds</span></h3>
+                <span class=" flex items-center space-x-2"><svg xmlns="http://www.w3.org/2000/svg" fill="#eb5757" viewBox="0 0 24 24" stroke-width="1.5" stroke="#eb5757" class="size-4">
+               <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+             </svg>${stars}</span>
+                 </div>
+                <p class=" text-gray-900 font-bold px-2 ">
+                    <span>${status}</span> 
                 </p>
-
-                 
-            
         </li>
     `;
     return template;
   }
+}
+
+export function toggle() {
+  modal.classList.toggle("hidden");
+}
+
+// Estoy descifrando este codigo porq se me filtraba todas las ciudades osea me salian como 10 y se repetian , pedi ayuda para ver que no se repitan.
+
+export function loadLocation(array, element) {
+  element.innerHTML = "";
+
+  // Arreglo para almacenar las combinaciones únicas de ciudad y país
+  let addedLocations = [];
+
+  array.forEach(function (item) {
+    // Combinamos ciudad y país en una sola cadena
+    const location = `${item.city}, ${item.country}`;
+
+    // Solo agregar si aún no se ha agregado esta combinación
+    if (!addedLocations.includes(location)) {
+      addedLocations.push(location); // Agregar a las combinaciones ya vistas
+
+      // Crear el template y agregarlo al HTML
+      const template = `
+        <li class="p-1 my-2 flex items-center space-x-2 cursor-pointer">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#4B5563" class="size-4">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+          </svg>
+          <span>${item.city}, ${item.country}</span>
+        </li>
+      `;
+      element.innerHTML += template;
+    }
+  });
 }
